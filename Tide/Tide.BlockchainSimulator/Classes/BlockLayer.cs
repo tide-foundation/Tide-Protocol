740027ab283f6f3e3fc943a3d9c24b8fd96ec103
Matthew Spencer <thrakmar@gmail.com>
2020-05-26 12:42:13 +1000

    Scaffold simulator project

---
+++ Tide/Tide.BlockchainSimulator/Classes/BlockLayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tide.BlockchainSimulator.Classes
+{
+    public class BlockLayer : IBlockLayer
+    {
+    }
+}
