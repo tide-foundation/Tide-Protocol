16f3309d051ea6dbaf6b03299c3846d337a0232b
José Lobo <jose@tide.org>
2020-05-27 19:15:04 +1000

    fix errors in ork

--- Tide/Tide.Ork/Controllers/AuthenticationController.cs
+++ Tide/Tide.Ork/Controllers/AuthenticationController.cs
@@ -3,25 +3,31 @@ using Tide.Library.Models;
 using Tide.Library.Models.Interfaces;
 using Tide.Ork.Classes;
 
-namespace Tide.Ork.Controllers {
+namespace Tide.Ork.Controllers
+{
     [Route("api/auth")]
     [ApiController]
-    public class AuthenticationController : ControllerBase {
+    public class AuthenticationController : ControllerBase
+    {
         private readonly IOrkAuthentication _orkAuthentication;
 
-        public AuthenticationController(OrkAuthentication orkAuthentication) {
+        public AuthenticationController(OrkAuthentication orkAuthentication)
+        {
             _orkAuthentication = orkAuthentication;
         }
 
-        public IActionResult GetNodes(JsonData model) {
+        public IActionResult GetNodes(AuthenticationModel model)
+        {
             return new JsonResult(_orkAuthentication.GetNodes(model));
         }
 
-        public IActionResult PostFragment(JsonData model) {
+        public IActionResult PostFragment(AuthenticationModel model)
+        {
             return new JsonResult(_orkAuthentication.PostFragment(model));
         }
 
-        public IActionResult GetFragment(JsonData model) {
+        public IActionResult GetFragment(AuthenticationModel model)
+        {
             return new JsonResult(_orkAuthentication.GetFragment(model));
         }
     }
