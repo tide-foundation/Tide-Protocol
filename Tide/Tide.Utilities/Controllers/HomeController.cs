de5b7b25195d87c19ba6b301248734bcb383084b
Matthew Spencer <thrakmar@gmail.com>
2020-06-08 09:40:13 +1000

    Remade the client project as Tide.Utilities

---
+++ Tide/Tide.Utilities/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Tide.Utilities.Models;
+
+namespace Tide.Utilities.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dauth()
+        {
+            return View();
+        }
+
+        public IActionResult Explorer()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Tide/Tide.Utilities/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tide.Utilities.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
