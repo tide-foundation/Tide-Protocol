1664bb81f8c9c2bd4860252bb76269e15a278084
Matthew Spencer <thrakmar@gmail.com>
2020-06-11 16:21:38 +1000

    Removed Tide.Utilities and added Onboarding diagram UML code

--- Tide/Tide.Ork/Publish.ps1
+++ Tide/Tide.Ork/Publish.ps1
@@ -2,18 +2,25 @@ az login
 
 $location = Get-Location
 $location = $location.Path
+$group = "Tide"
 
 $restartApp = {
     param ($number)
-    Restart-AzureRmWebApp -ResourceGroupName Tide -Name ork-$number
+    Restart-AzureRmWebApp -ResourceGroupName $group -Name ork-$number
 }
 
 For ($i = 0; $i -le 10; $i++) {
-    $pwd = az webapp deployment list-publishing-profiles --name ork-$i --resource-group Tide --query '[].userPWD' -o tsv
+    az webapp config appsettings set --resource-group $group --name ork-$i --settings WEBSITE_RUN_FROM_PACKAGE="0"
+}
+
+For ($i = 0; $i -le 10; $i++) {
+    $pwd = az webapp deployment list-publishing-profiles --name ork-$i --resource-group $group --query '[].userPWD' -o tsv
     $pwd = $pwd[0];
     $project = -join ($location, '\Tide.Ork.csproj');
     $profile = -join ("ork-", $i, " - Web Deploy.pubxml");
     
+
+
     dotnet publish $project /p:PublishProfile=$profile /p:Password=$pwd -v q
     
     if ($LASTEXITCODE -eq 0) {
