7b71f6c9abcd4e85d8b6c49205dc40c9ebf328d5
José Lobo <jose@tide.org>
2020-06-25 14:13:48 +1000

    change dev blockchain fit more production blockchain

--- Tide/Tide.Ork/Classes/MemoryKeyManager.cs
+++ Tide/Tide.Ork/Classes/MemoryKeyManager.cs
@@ -9,36 +9,48 @@ namespace Tide.Ork.Classes
 {
     public class MemoryKeyManager : IKeyManager
     {
-        private readonly Dictionary<Guid, KeyVault> _items;
+        private readonly Dictionary<string, string> _items;
 
         public MemoryKeyManager()
         {
-            _items = new Dictionary<Guid, KeyVault>();
+            _items = new Dictionary<string, string>();
         }
 
         public Task<bool> Exist(Guid user)
         {
-            return Task.FromResult(_items.ContainsKey(user));
+            return Task.FromResult(_items.ContainsKey(user.ToString()));
         }
 
         public Task<BigInteger> GetAuthShare(Guid user)
         {
-            return Task.FromResult(_items.ContainsKey(user) ? _items[user].AuthShare : BigInteger.Zero);
+            var usr = user.ToString();
+            if (!_items.ContainsKey(usr))
+                return Task.FromResult(BigInteger.Zero);
+             
+            return Task.FromResult(KeyVault.Parse(_items[usr]).AuthShare);
         }
 
         public Task<AesKey> GetSecret(Guid user)
         {
-            return Task.FromResult(_items.ContainsKey(user) ? _items[user].Secret : null);
+            var usr = user.ToString();
+            if (!_items.ContainsKey(usr))
+                return Task.FromResult<AesKey>(null);
+
+            return Task.FromResult(KeyVault.Parse(_items[usr]).Secret);
         }
 
         public Task<KeyVault> GetByUser(Guid user)
         {
-            return Task.FromResult(_items.ContainsKey(user) ? _items[user] : null);
+            var usr = user.ToString();
+            if (!_items.ContainsKey(usr))
+                return Task.FromResult<KeyVault>(null);
+
+            return Task.FromResult(KeyVault.Parse(_items[usr]));
         }
 
         public Task<TideResponse> SetOrUpdate(KeyVault account)
         {
-            _items[account.User] = account;
+            _items[account.User.ToString()] = account.ToString();
             return Task.FromResult(new TideResponse());
         }
     }
