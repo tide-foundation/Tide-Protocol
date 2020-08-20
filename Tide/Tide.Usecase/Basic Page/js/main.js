b4303d3b8799d8560465c3d40b8ef10f08e583ae
Matt <thrakmar@gmail.com>
2020-08-20 10:19:32 +1000

    Test page

--- Tide/Tide.Usecase/Basic Page/js/main.js
+++ Tide/Tide.Usecase/Basic Page/js/main.js
@@ -1,26 +1,43 @@
-var orkUrls = [...Array(3)].map((_, i) => `https://ork-${i}.azurewebsites.net`);
-var vendorUrl = "https://tidevendor.azurewebsites.net/";
-
-// var orkUrls = [...Array(3)].map((_, i) => "http://localhost:500" + (i + 1));
-// var vendorUrl = "http://127.0.0.1:6001";
-
-var tide = new Tide("VendorId", vendorUrl, orkUrls, "publickey");
-
 var user = "admin";
 var pass = "123456";
 var email = "tmp@tide.org";
 
-console.log(tide);
+var localBtn = document.getElementById("local");
+var remoteBtn = document.getElementById("remote");
+var runBtn = document.getElementById("run");
+var logDiv = document.getElementById("log");
 
-tide
-  .registerV2(user, pass, email, orkUrls)
-  .then((registerResponse) => {
-    console.log("Registration Successful");
-    tide
-      .loginV2(user, pass, orkUrls)
-      .then((loginResponse) => {
-        console.log("Login Successful");
-      })
-      .catch((loginError) => console.log(`Login Failed eith error: `, loginError));
-  })
-  .catch((registerError) => console.log(`Register Failed eith error: `, registerError));
+var orkUrls = [...Array(3)].map((_, i) => "http://localhost:500" + (i + 1));
+var vendorUrl = "http://127.0.0.1:6001";
+
+localBtn.onclick = function () {
+  orkUrls = [...Array(3)].map((_, i) => "http://localhost:500" + (i + 1));
+  vendorUrl = "http://127.0.0.1:6001";
+};
+
+remoteBtn.onclick = function () {
+  orkUrls = [...Array(3)].map((_, i) => `https://ork-${i}.azurewebsites.net`);
+  vendorUrl = "https://tidevendor.azurewebsites.net/";
+};
+
+runBtn.onclick = function () {
+  var tide = new Tide("VendorId", vendorUrl, orkUrls, "publickey");
+
+  tide
+    .registerV2(user, pass, email, orkUrls)
+    .then((registerResponse) => {
+      log("Registration Successful");
+      tide
+        .loginV2(user, pass, orkUrls)
+        .then((loginResponse) => {
+          log("Login Successful");
+        })
+        .catch((loginError) => log(`Login Failed`, loginError));
+    })
+    .catch((registerError) => log(`Register Failed`, registerError));
+};
+
+function log(msg, error) {
+  error != null ? console.log(msg, error) : console.log(msg);
+  logDiv.innerHTML += `<p class="${error != null ? "error" : "success"}">${msg}</p>`;
+}
