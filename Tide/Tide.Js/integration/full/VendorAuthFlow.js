0c092ebdaa5ae64c523e8b40cd4830b4006f714b
Matthew Spencer <thrakmar@gmail.com>
2020-09-24 13:09:53 +1000

    rule update test

--- Tide/Tide.Js/integration/full/VendorAuthFlow.js
+++ Tide/Tide.Js/integration/full/VendorAuthFlow.js
@@ -3,13 +3,16 @@ import request from "superagent";
 import Cipher from "../../src/Cipher";
 //import "../../dist/tide";
 
-var orkUrls = [...Array(13)].map((_, i) => `https://ork-${i}.azurewebsites.net/`);
-var vendorUrl = "https://tidevendor.azurewebsites.net/";
+// var orkUrls = [...Array(3)].map((_, i) => `https://ork-${i}.azurewebsites.net/`);
+// var vendorUrl = "https://tidevendor.azurewebsites.net/";
 
-// var orkUrls = [...Array(3)].map((_, i) => "http://localhost:500" + (i + 1));
-// var vendorUrl = "http://127.0.0.1:6001";
+var orkUrls = [...Array(3)].map((_, i) => "http://localhost:500" + (i + 1));
+var vendorUrl = "http://127.0.0.1:6001";
 
-var tide = new Tide("VendorId", vendorUrl, orkUrls, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANvjzMxmyjGxse3fwkqajZxhf088eQRgS4l9wKsnm+A2+HRLt/4n6lA0cO6pmBqB9Le72HFSQ1s9cjv6HF3O2m", ["field1", "field2"]);
+var tide = new Tide("VendorId", vendorUrl, orkUrls, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANvjzMxmyjGxse3fwkqajZxhf088eQRgS4l9wKsnm+A2+HRLt/4n6lA0cO6pmBqB9Le72HFSQ1s9cjv6HF3O2m", [
+  { name: "field1", condition: "true" },
+  { name: "field2", condition: "true" },
+]);
 
 var user = "admin";
 var pass = "123456";
@@ -23,17 +26,6 @@ async function main() {
   try {
     await tide.initialize();
 
-    // const selectedOrks = tide.orks
-    //   .sort(() => 0.5 - Math.random())
-    //   .slice(0, 3)
-    //   .map((o) => o.url);
-    //  const selectedOrks = orkUrls
-    //   .sort(() => 0.5 - Math.random())
-    //   .slice(0, 3);
-
-    // console.log(selectedOrks);
-
-    // Needs to return vuid, authkey, cvk
     var signUpResult = await tide.registerV2(user, pass, email, orkUrls);
 
     var userData = {
@@ -45,10 +37,13 @@ async function main() {
 
     await request.post(`${vendorUrl}/account`).send(userData);
 
-    // var partialDecrypt = await request.get(`${vendorUrl}/BackendTest/${userData.id}/true`);
-    // console.log(partialDecrypt.body);
+    var rules = (await tide.getRules())[0];
+    var ruleToEdit = rules[0];
+    //ruleToEdit.condition = `[{"field":"DateInfo.Day","operator":"==","value":1,"level":0},{"union":"&&","field":"DateInfo.Today","operator":">","value":"21/01/2036","level":0}]`;
+    await tide.updateRule(ruleToEdit);
 
     var login = await tide.loginV2(user, pass, orkUrls);
+
     console.log("Done login");
   } catch (error) {
     console.log(error);
