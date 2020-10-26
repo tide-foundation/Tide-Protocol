83b24924fd27d556100e63cac4b161f66856b77d
Matthew Spencer <thrakmar@gmail.com>
2020-10-26 14:02:20 +1000

    Cleaned up the vendor client

--- Tide/Tide.Vendor/Client/src/App.vue
+++ Tide/Tide.Vendor/Client/src/App.vue
@@ -1,40 +1,105 @@
 <template>
-  <div id="app">
-    <Status></Status>
-    <Loading></Loading>
+  <div id="home">
+    <h1>THIS IS AN EXAMPLE VENDOR WEBSITE</h1>
+    <h2>PLEASE LOGIN OR REGISTER</h2>
+    <span id="tide"></span>
 
-    <router-view />
+    <button @click="getProtected" id="protected-data-btn">Request Protected Data</button>
+    <p :style="{ color: error ? 'red' : 'black' }" v-html="protectedData"></p>
   </div>
 </template>
 
+<script src="tide-button.js"></script>
 <script>
-import Loading from "@/components/Loading.vue";
-import Status from "@/components/Status.vue";
-
+import request from "superagent";
 export default {
-  components: {
-    Loading,
-    Status,
-  },
+  name: "Home",
+
   data() {
-    return {};
+    return {
+      jwt: "",
+      protectedData: "",
+      error: false,
+      config: {
+        homeUrl: process.env.VUE_APP_RETURN_URL,
+        hashedReturnUrl: process.env.VUE_APP_RETURN_URL_HASH,
+        serverUrl: process.env.VUE_APP_SERVER_URL,
+        chosenOrk: process.env.VUE_APP_CHOSEN_ORK,
+        vendorPublic: process.env.VUE_APP_VENDOR_PUBLIC,
+      },
+    };
+  },
+  created() {
+    window.addEventListener("tide-auth", async (e) => {
+      var data = { vuid: e.detail.data.vuid, tideToken: e.detail.data.tideToken, publicKey: e.detail.data.cvkPublic };
+
+      this.jwt = (await request.post(`${this.config.serverUrl}/Authentication/register`).send(data)).text;
+    });
+    Tide.init(this.config);
+  },
+  methods: {
+    async getProtected() {
+      try {
+        this.error = false;
+        this.protectedData = "";
+        var test = await request.get(`${this.config.serverUrl}/Authentication`).set("Authorization", `Bearer ${this.jwt}`);
+
+        this.protectedData = `Data successfully fetched for user: <strong> ${test.text}</strong>`;
+      } catch (error) {
+        this.error = true;
+        this.protectedData = `Failed gathering data`;
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap");
 body,
 html {
+  font-family: "Montserrat", sans-serif;
   width: 100%;
   padding: 0;
   margin: 0;
 }
-
-#app {
+#home {
   width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+}
+
+#protected-data-btn {
+  width: 200px;
+  margin: 20px auto;
+}
+
+.loading {
+  text-align: center;
+  z-index: 1000000000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(-360deg);
+    }
+  }
 }
 </style>
--- Tide/Tide.Vendor/Client/src/main.js
+++ Tide/Tide.Vendor/Client/src/main.js
@@ -1,21 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
-import "../src/assets/scss/main.scss";
 
 Vue.config.productionTip = false;
-Vue.prototype.$bus = new Vue();
-
-Vue.prototype.$loading = (a, m) =>
-  store.commit("UPDATE_LOADING", {
-    active: a,
-    text: m,
-  });
 
 new Vue({
-  router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
