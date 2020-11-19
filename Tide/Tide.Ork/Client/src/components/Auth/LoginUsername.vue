f539bae6f7d2a6a45d2318e9e3bf0ec10425ce0f
Matt <thrakmar@gmail.com>
2020-11-19 19:01:30 +1000

    Qr code on enclave

--- Tide/Tide.Ork/Client/src/components/Auth/LoginUsername.vue
+++ Tide/Tide.Ork/Client/src/components/Auth/LoginUsername.vue
@@ -1,66 +1,83 @@
 <template>
-  <span>
-    <h2>Sign in</h2>
-    <form @submit.prevent="$parent.changeMode('LoginPassword')">
-      <input type="text" required class="mt-50" v-model="user.username" placeholder="Username" />
+    <span>
+        <h2>Sign in</h2>
+        <form @submit.prevent="$parent.changeMode('LoginPassword')">
+            <input type="text" required class="mt-50" v-model="user.username" placeholder="Username" />
 
-      <div class="action-row mt-50">
-        <p @click="$parent.changeMode('Register')">Create Account</p>
-        <button>Next</button>
-      </div>
-      <!-- <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" />
-      </div>
-      <div class="form-group" id="go-to-account-box">
-        <label for="go-to-account">Go to account</label>
-        <input type="checkbox" id="go-to-account" v-model="user.goToDashboard" />
-      </div> -->
-      <!-- <div class="form-group">
-        <button type="submit">LOGIN</button>
-      </div>
-      <p>OR</p>
-      <p class="link" @click="$parent.changeMode('Register')">Register</p> -->
-      <div class="advanced-options" @click="$parent.changeMode('ChangeOrk')"><p>Advanced Options</p></div>
-    </form>
-  </span>
+            <div class="action-row mt-50">
+                <p @click="$parent.changeMode('Register')">Create Account</p>
+                <button>Next</button>
+            </div>
+
+            <div class="advanced-options" @click="$parent.changeMode('ChangeOrk')"><p>Advanced Options</p></div>
+        </form>
+        <div id="qr">
+            <canvas id="canvas"></canvas>
+            <span> Tide Shield</span>
+        </div>
+    </span>
 </template>
 
 <script>
+var QRCode = require("qrcode");
 export default {
-  props: ["user"],
-  methods: {
-    async login() {
-      try {
-        this.$loading(true, "Logging in...");
-        var loginResult = await this.$store.dispatch("loginAccount", this.user);
-
-        await this.$store.dispatch("finalizeAuthentication", loginResult);
-      } catch (error) {
-        this.$bus.$emit("show-error", error);
-      } finally {
-        this.$loading(false, "");
-      }
+    props: ["user"],
+    mounted() {
+        var canvas = document.getElementById("canvas");
+        console.log(canvas);
+        QRCode.toCanvas(canvas, "From Tide, with love", { color: { dark: "#1e7ec2" }, margin: 0, errorCorrectionLevel: "L" });
     },
-  },
+    methods: {
+        async login() {
+            try {
+                this.$loading(true, "Logging in...");
+                var loginResult = await this.$store.dispatch("loginAccount", this.user);
+
+                await this.$store.dispatch("finalizeAuthentication", loginResult);
+            } catch (error) {
+                this.$bus.$emit("show-error", error);
+            } finally {
+                this.$loading(false, "");
+            }
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
+#qr {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    #canvas {
+        background: red;
+        margin-bottom: 0px;
+    }
+    span {
+        font-size: 10px;
+        margin-top: 5px;
+        color: #000000;
+        z-index: 20;
+    }
+}
 #filler {
-  height: 6px;
+    height: 6px;
 }
 #go-to-account-box {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 
-  input {
-    width: 20px;
-    padding-left: 0px;
-    margin-left: 0px;
-  }
+    input {
+        width: 20px;
+        padding-left: 0px;
+        margin-left: 0px;
+    }
 }
 #go-to-account {
-  text-align: left;
+    text-align: left;
 }
 </style>
--- Tide/Tide.Ork/Client/src/views/Auth.vue
+++ Tide/Tide.Ork/Client/src/views/Auth.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="auth">
-    <div class="content">
-      <span v-if="!$store.getters.loggedIn">
-        <transition name="fade" mode="out-in">
-          <LoginUsername :user="user" v-if="mode == 'LoginUsername'"></LoginUsername>
-          <LoginPassword :user="user" v-if="mode == 'LoginPassword'"></LoginPassword>
-          <ChangeOrk :user="user" v-if="mode == 'ChangeOrk'"></ChangeOrk>
-          <SelectOrks :user="user" v-if="mode == 'SelectOrks'"></SelectOrks>
-          <Register :user="user" v-else-if="mode == 'Register'"></Register>
-          <ForgotSend :user="user" v-else-if="mode == 'ForgotSend'"></ForgotSend>
-          <ForgotReconstruct :user="user" v-else-if="mode == 'ForgotReconstruct'"></ForgotReconstruct>
-          <ForgotReset :user="user" v-else-if="mode == 'ForgotReset'"></ForgotReset>
-        </transition>
+    <div id="auth">
+        <div class="content">
+            <span v-if="!$store.getters.loggedIn">
+                <transition name="fade" mode="out-in">
+                    <LoginUsername :user="user" v-if="mode == 'LoginUsername'"></LoginUsername>
+                    <LoginPassword :user="user" v-if="mode == 'LoginPassword'"></LoginPassword>
+                    <ChangeOrk :user="user" v-if="mode == 'ChangeOrk'"></ChangeOrk>
+                    <SelectOrks :user="user" v-if="mode == 'SelectOrks'"></SelectOrks>
+                    <Register :user="user" v-else-if="mode == 'Register'"></Register>
+                    <ForgotSend :user="user" v-else-if="mode == 'ForgotSend'"></ForgotSend>
+                    <ForgotReconstruct :user="user" v-else-if="mode == 'ForgotReconstruct'"></ForgotReconstruct>
+                    <ForgotReset :user="user" v-else-if="mode == 'ForgotReset'"></ForgotReset>
+                </transition>
 
-        <p>{{ status }}</p>
-      </span>
-      <Logout v-else></Logout>
+                <p>{{ status }}</p>
+            </span>
+            <Logout v-else></Logout>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -31,49 +31,49 @@ import ForgotSend from "../components/Auth/ForgotSend.vue";
 import ForgotReconstruct from "../components/Auth/ForgotReconstruct.vue";
 import ForgotReset from "../components/Auth/ForgotReset.vue";
 export default {
-  components: {
-    ChangeOrk,
-    SelectOrks,
-    LoginUsername,
-    LoginPassword,
-    Register,
-    Logout,
-    ForgotSend,
-    ForgotReconstruct,
-    ForgotReset,
-  },
+    components: {
+        ChangeOrk,
+        SelectOrks,
+        LoginUsername,
+        LoginPassword,
+        Register,
+        Logout,
+        ForgotSend,
+        ForgotReconstruct,
+        ForgotReset,
+    },
 
-  data() {
-    return {
-      status: "",
-      mode: "LoginUsername",
-      user: {
-        username: this.$store.getters.debug ? "matt@tide.org" : "",
-        password: this.$store.getters.debug ? "password" : "",
-        confirm: this.$store.getters.debug ? "password" : "",
-        goToDashboard: false,
-        homeOrk: "http://172.26.17.60:8081/",
-        recoveryEmails: [this.$store.getters.debug ? "matt@tide.org" : ""],
-        selectedOrks: [],
-        frags: [],
-      },
-    };
-  },
-  created() {
-    this.user.selectedOrks = this.$store.getters.orks;
-    this.user.frags = [...Array(this.$store.getters.orks.length)].map((_, i) => "");
-  },
-  methods: {
-    setStatus(msg) {
-      this.status = msg;
+    data() {
+        return {
+            status: "",
+            mode: "LoginUsername",
+            user: {
+                username: this.$store.getters.debug ? "matt@tide.org" : "",
+                password: this.$store.getters.debug ? "password" : "",
+                confirm: this.$store.getters.debug ? "password" : "",
+                goToDashboard: false,
+                homeOrk: "http://172.26.17.60:8081/",
+                recoveryEmails: [this.$store.getters.debug ? "matt@tide.org" : ""],
+                selectedOrks: [],
+                frags: [],
+            },
+        };
     },
-    changeMode(mode) {
-      this.mode = mode;
+    created() {
+        this.user.selectedOrks = this.$store.getters.orks;
+        // this.user.frags = [...Array(this.$store.getters.orks.length)].map((_, i) => "");
     },
-    setUser(user) {
-      this.$store.commit("SET_USER", user);
+    methods: {
+        setStatus(msg) {
+            this.status = msg;
+        },
+        changeMode(mode) {
+            this.mode = mode;
+        },
+        setUser(user) {
+            this.$store.commit("SET_USER", user);
+        },
     },
-  },
 };
 </script>
 
