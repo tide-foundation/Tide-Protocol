33302d65eab80380d9d5270f7e282f666ea8cac4
Matt <thrakmar@gmail.com>
2021-09-08 17:31:37 +1000

    Client rename

---
+++ Tide/Tide.Ork/Client/src/store/main.js
@@ -0,0 +1,13 @@
+import { reactive, readonly } from "vue";
+export class Store {
+    constructor() {
+        let data = this.data();
+        this.setup(data);
+        this.state = reactive(data);
+    }
+    setup(data) { }
+    get getState() {
+        return readonly(this.state);
+    }
+}
+//# sourceMappingURL=main.js.map
