cad24e1de507cf38c7440067f774761ce5f2b54d
Matthew Spencer <thrakmar@gmail.com>
2021-11-12 17:59:27 +1000

    Dynamic form feature

--- Tide/Tide.Ork/Enclave/src/views/Form.vue
+++ Tide/Tide.Ork/Enclave/src/views/Form.vue
@@ -28,7 +28,18 @@ import Base from "@/assets/ts/Base";
 // @ts-ignore
 import MetaField from "@/assets/ts/MetaField";
 // @ts-ignore
+import Num64 from "../../../../Tide.Js/src/Num64";
+// @ts-ignore
 import { C25519Key } from "../../../../Tide.Js/src/export/TideAuthentication";
+
+interface Field {
+  Name: string;
+  Value: string;
+}
+
+interface DynamicForm {
+  [key: string]: any;
+}
 
 export default class Form extends Base {
   fields: any[] = [];
@@ -40,16 +51,24 @@ export default class Form extends Base {
     this.key = C25519Key.fromString(this.mainStore.getState.account!.encryptionKey);
     this.formData = this.mainStore.getState.config.formData;
 
-    this.encrypted = this.formData.type == "modify";
+    var structure = this.formData.structure as any[];
+    var userData = this.formData.data as any[];
+    console.log(structure);
+    console.log(userData);
+    structure.forEach((field: Field) => {
+      var v = userData.find((d) => d.key == field.Name);
+      var val = v != null && v.value != "" ? v.value : "";
 
-    this.fields = MetaField.fromModel(this.formData.data, this.encrypted, this.formData.validation, this.formData.classification, this.formData.tags);
+      var metaField = MetaField.fromText(field.Name, val, val != "");
+      if (val != "") metaField.decrypt(this.key);
 
-    for (const field of this.fields) {
-      const splitCamel = field.field.replace(/([a-z])([A-Z])/g, "$1 $2") as string;
-      field.friendlyName = `${splitCamel[0].toUpperCase()}${splitCamel.substring(1)}`;
+      metaField.tag = Num64.seed("__vendor__");
 
-      if (this.encrypted) field.decrypt(this.key);
-    }
+      const splitCamel = metaField.field.replace(/([a-z])([A-Z])/g, "$1 $2") as string;
+      metaField.friendlyName = `${splitCamel[0].toUpperCase()}${splitCamel.substring(1)}`;
+
+      this.fields.push(metaField);
+    });
   }
 
   async update() {
